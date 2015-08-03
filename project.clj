58f4c3d75a629bf90d190fde8ce15cbdd38ec624
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-08-03 14:53:35 +0100

    yoyo module for resources with a .close method

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject yoyo.closeable "0.0.1"
+  :description "yoyo module for closeable resources"
+  :url "https://github.com/employeerepublic/yoyo-closeable"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/tools.logging "0.3.1"]]
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.7.0"]]}})
---
+++ src/yoyo/closeable.clj
@@ -0,0 +1,14 @@
+(ns yoyo.closeable
+  (:require [clojure.tools.logging :as log]))
+
+(defn with-closeable
+  [label constructor-fn opts f]
+  (log/infof "Starting %s with opts: %s..." label (prn-str opts))
+  (let [obj (constructor-fn opts)]
+    (log/infof "Started %s." label)
+    (try
+      (f obj)
+      (finally
+        (log/infof "Stopping %s..." label)
+        (.close obj)
+        (log/infof "Stopped %." label)))))
