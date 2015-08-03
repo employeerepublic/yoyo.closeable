a4d83234ea2088a04f7d6bed4c96bf30ca5603e6
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2015-08-03 15:06:25 +0100

    remove need for partial application

--- src/yoyo/closeable.clj
+++ src/yoyo/closeable.clj
@@ -2,13 +2,14 @@
   (:require [clojure.tools.logging :as log]))
 
 (defn with-closeable
-  [label constructor-fn opts f]
-  (log/infof "Starting %s with opts: %s..." label (prn-str opts))
-  (let [obj (constructor-fn opts)]
-    (log/infof "Started %s." label)
-    (try
-      (f obj)
-      (finally
-        (log/infof "Stopping %s..." label)
-        (.close obj)
-        (log/infof "Stopped %." label)))))
+  [label constructor-fn]
+  (fn [opts f]
+    (log/infof "Starting %s with opts: %s..." label (prn-str opts))
+    (let [obj (constructor-fn opts)]
+      (log/infof "Started %s." label)
+      (try
+        (f obj)
+        (finally
+          (log/infof "Stopping %s..." label)
+          (.close obj)
+          (log/infof "Stopped %." label))))))
